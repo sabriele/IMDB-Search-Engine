e12a1da45eddaf52721fc31eca4763b4a2b61176
Sabrina Tjeng <sabriele@users.noreply.github.com>
2014-11-04 14:36:14 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// DEPENDENCIES
+var express    = require("express"),
+	request    = require("request");
+
+// APP CONFIG
+var app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/node_modules/semantic-ui/dist"));
+
+// ROUTES
+app.get("/", function (req, res) {
+    res.render("index");
+});
+
+app.get("/results", function (req, res) {
+	var url = "http://www.omdbapi.com/?s=" + req.query.search;
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body);
+			res.render("results", { data: data });
+		}
+	});
+});
+
+// SERVER
+var PORT_NUM = 3000;
+var SERVER_START_MSG = "*Server is up and running on port " + PORT_NUM + "!*";
+
+app.listen(PORT_NUM, function () {
+	console.log(SERVER_START_MSG);
+});
+
