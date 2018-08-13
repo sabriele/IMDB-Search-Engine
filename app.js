72be24205f724de9ab483480927c015deec5a424
Sabrina Tjeng <sabriele@users.noreply.github.com>
2018-08-14 04:40:53 +0800

    Refactor API script and update API URLs to HTTPS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // DEPENDENCIES
-var express    = require("express"),
-	request    = require("request");
+var express = require("express"),
+	request = require("request");
 
 // APP CONFIG
 var app = express();
@@ -9,13 +9,14 @@ app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/node_modules/semantic-ui/dist"));
 
 // ROUTES
-app.get("/", function (req, res) {
-    res.render("index");
+app.get("/", function(req, res) {
+	res.render("index");
 });
 
-app.get("/results", function (req, res) {
-	var url = "http://www.omdbapi.com/?s=" + req.query.search + "&apikey=thewdb";
-	request(url, function (error, response, body) {
+app.get("/results", function(req, res) {
+	var url =
+		"https://www.omdbapi.com/?s=" + req.query.search + "&apikey=thewdb";
+	request(url, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 			res.render("results", { data: data });
@@ -27,7 +28,6 @@ app.get("/results", function (req, res) {
 var PORT_NUM = process.env.PORT || 3000;
 var SERVER_START_MSG = "*Server is up and running on port " + PORT_NUM + "!*";
 
-app.listen(PORT_NUM, function () {
+app.listen(PORT_NUM, function() {
 	console.log(SERVER_START_MSG);
 });
-
